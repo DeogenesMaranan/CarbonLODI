313fbf232722f7a2002666dfc006c543ba8694d5
JARON LOUISE ARQUILLO <22-03531@g.batstate-u.edu.ph>
2023-05-22 18:31:29 +0800

    modified main

--- main.py
+++ main.py
@@ -7,32 +7,29 @@ try:
 except ModuleNotFoundError:  # Handle the missing module error
     print("The 'miscellaneous.py' file is missing.")
     print("Please download the latest version of the Repository")
-    time.sleep(4)
+    time.sleep(3)
     sys.exit(1002)
-    
-def main():
-    while True:  # Runs the Main Menu in loop
-        os.system('cls')
-        print(Constants().logo)
-        Constants.print_random_recommendation()
-        choice = input(Constants().main_menu)
-        print("")
 
-        os.system('cls')  # Clear screen before processing user choice
+while True:  # Runs the Main Menu in loop
+    os.system('cls')
+    print(Constants().logo)
+    Constants.print_random_recommendation()
+    choice = input(Constants().main_menu)
+    print("")
 
-        if choice == '1':
-            current_user = AccountManager().register()
+    os.system('cls')  # Clear screen before processing user choice
+
+    if choice == '1':
+        current_user = AccountManager().register()
+        input("\nPress any key to continue...")
+    elif choice == '2':
+        current_user = AccountManager().login()
+        if current_user is None:
             input("\nPress any key to continue...")
-        elif choice == '2':
-            current_user = AccountManager().login()
-            if current_user is None:
-                input("\nPress any key to continue...")
-            else:
-                AccountManager().show_home(current_user)
-        elif choice == '0':
-            break
         else:
-            print("Invalid choice. Please try again.")
-            input("Press Enter to continue...")
-
-main()
+            AccountManager().show_home(current_user)
+    elif choice == '0':
+        break
+    else:
+        print("Invalid choice. Please try again.")
+        input("Press Enter to continue...")
